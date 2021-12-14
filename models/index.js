adafc13cdd5f7fd9096344833fa4cefa31f90089
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-12-14 15:27:25 -0700

    category routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+//ask armando if this is right
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_tag'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'product_tag'
+});
 
 module.exports = {
   Product,
